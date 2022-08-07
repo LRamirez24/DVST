7d61b491896d01c88abc97e0408db0948818a213
Luis Ramirez <lrxd2025@gmail.com>
2022-08-07 15:29:55 -0700

    fixed icon for tabs

--- components/layout.js
+++ components/layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo2.svg" />
         <meta
           name="description"
           content=" DVST Quantum Finance
