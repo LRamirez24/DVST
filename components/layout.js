18bb3ef9afb55776abe87f0d04f1be7cc9c3c12d
Luis Ramirez <lrxd2025@gmail.com>
2022-10-07 13:47:18 -0700

    edits to mobile example

--- components/layout.js
+++ components/layout.js
@@ -21,44 +21,37 @@ export default function Layout({ children, home }) {
           Secure Exchange Ecosystem. Learn More Here"
         />
 
-        {/* <meta
+        <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle,
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        /> */}
+        />
 
 
        
 
         <meta name="og:title" content={siteTitle} />
 
-        <meta
-          property="og:description"
-          content="DVST Quantum Finance
-          A Decentralized, Cross-Chain, Post Quantum
-          Secure Exchange Ecosystem. Learn More Here"
-        />
+       
 
         <meta property="og:type" content="website"/>
 
         <meta property="og:url" content="https://dvst.io"/>
 
 
-        {/* <meta name="twitter:card" content="summary_large_image" /> */}
+        <meta name="twitter:card" content="summary_large_image" />
 
         
 
-        {/* <meta property="og:url" content="your url" />
-        <meta property="og:type" content="website" /> */}
+   
       
    
      
 
         {/* <meta property="og:image" content={"https://images.unsplash.com/photo-1627645812426-67ce7b0a7a81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"}/> */}
 
-        {/* <meta property="og:image" content="https://images.unsplash.com/photo-1627645812426-67ce7b0a7a81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"/>
-         */}
+       
 
 
 
